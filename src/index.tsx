5d38ac01a3c1ec886e50d94ec68076993bb575e8
taeo <ohtk1119@gmail.com>
2024-04-17 16:47:06 +0900

    FIX 경로 문제로 인한 오류 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Test from "test";
+import Test from "./test";
 
-const App = () => <Test />;
+const App = () => (
+  <div>
+    <Test />
+  </div>
+);
 
 const container = document.getElementById("wrap") as HTMLElement;
 const root = ReactDOM.createRoot(container);
